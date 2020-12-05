2cf03380e0fd012c120c352663923f4a78cd7a9c
dollarkillerx <adapawang@gmail.com>
2020-12-05 23:54:06 +0800

    UDP Async

--- s03/std_3/src/main.rs
+++ s03/std_3/src/main.rs
@@ -2,6 +2,11 @@ use async_std::task;
 use std::time::Duration;
 
 fn main() {
+    // test1();
+    test2();
+}
+
+fn test1() {
     println!("Hello, world!");
     let main = task::spawn(async_main());
     task::block_on(main);
@@ -11,6 +16,24 @@ fn main() {
     });
     task::block_on(p);
     println!("panic test");
+}
+
+fn test2() {
+    let t = task::spawn(pcr());
+    task::block_on(t);
+}
+
+async fn pcr() {
+    let _h = task::spawn(async move {
+        loop {
+            task::sleep(Duration::from_secs(1)).await;
+            println!("abc");
+        }
+    });
+
+    loop {
+        task::sleep(Duration::from_secs(1)).await;
+    }
 }
 
 async fn async_main() {
