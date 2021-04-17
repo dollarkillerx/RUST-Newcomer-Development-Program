6fae5529b8d47859cab2207cf9a89177e5a7dc53
dollarkillerx <adapawang@gmail.com>
2021-04-17 23:07:17 +0800

    udp fn

--- s13.5/demo1/src/main.rs
+++ s13.5/demo1/src/main.rs
@@ -6,11 +6,42 @@ enum List<T> {
 
 fn main() {
     println!("Hello, world!");
-    let list: List<i32> = List::Cons(1, Box::new(List::Cons(16,Box::new(List::Nil))));
+    let list: List<i32> = List::Cons(1, Box::new(List::Cons(16, Box::new(List::Nil))));
     println!("list: {:?}", list);
 }
 
-enum List2{
+enum List2 {
     Empty,
-    Elem(i32, Box<List2>)
+    Elem(i32, Box<List2>),
 }
+
+struct CaCher<T>
+    where
+        T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> CaCher<T>
+    where T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> CaCher<T> {
+        CaCher{
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32{
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
