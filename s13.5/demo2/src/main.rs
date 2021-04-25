4daa2a5d0e83f1a55f1cb6747bab6055fab32c48
dollarkillerx <adapawang@gmail.com>
2021-04-25 14:26:51 +0800

    add more link

--- s13.5/demo2/src/main.rs
+++ s13.5/demo2/src/main.rs
@@ -2,31 +2,89 @@ use std::fmt::Debug;
 
 fn main() {
     println!("Hello, world!");
+    test_a();
 }
 
 // struct Link {
 //     head: Link
 // }
 
+// #[derive(Debug)]
+// enum Link<T>
+//     where T: Debug
+// {
+//     Empty,
+//     More(Box<Node<T>>),
+// }
+//
+// #[derive(Debug)]
+// struct Node<T>
+//     where T: Debug
+// {
+//     elem: T,
+//     next: Link<T>,
+// }
+//
+// impl <T> Link<T>
+//     where T: Debug
+// {
+//     fn new() -> Link<T> {
+//         Link::Empty
+//     }
+//
+//     fn append(&mut self, elem: T) {
+//         self
+//     }
+// }
+
 #[derive(Debug)]
-enum Link<T>
+struct Link<T>
     where T: Debug
 {
-    Empty,
-    More(Box<Node<T>>),
+    node: Option<Box<LinkNode<T>>>
 }
 
 #[derive(Debug)]
-struct Node<T>
+struct LinkNode<T>
     where T: Debug
 {
-    elem: T,
-    next: Link<T>,
+    node: T,
+    next: Option<Box<LinkNode<T>>>,
 }
 
-impl <T> Link<T>
+impl<T> Link<T>
     where T: Debug
 {
-    
+    fn new() -> Link<T> {
+        return Link {
+            node: None
+        };
+    }
 
+    fn append(&mut self, elem: T) {
+        let mut ps = self.node.as_mut();
+        if let None = ps {
+            self.node = Some(Box::new(LinkNode { node: elem, next: None }));
+            return;
+        }
+
+        println!("aaa");
+        // loop {
+        //     if let Some(p) = ps {
+        //         ps = p.next.as_mut();
+        //     } else {
+        //         ps.next = Some(Box::new(LinkNode { node: elem, next: None }));
+                // break;
+            // }
+        // }
+    }
+}
+
+fn test_a() {
+    let mut c = Link::new();
+    for i in 0..10 {
+        c.append(i);
+    }
+
+    println!("r : {:?}", c);
 }
