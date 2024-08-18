41eb8524a98903f17cd528267797d69edddac5f8
Леони́д Ильи́ч Бре́жнев <google@gmail.com>
2024-08-18 09:13:40 +0900

    add s01_new

--- s09_salvo/src/main.rs
+++ s09_salvo/src/main.rs
@@ -7,6 +7,7 @@ mod storage;
 mod enums;
 mod app_state;
 
+use std::env;
 use std::sync::Arc;
 use salvo::prelude::*;
 use crate::app_state::AppState;
@@ -15,9 +16,11 @@ use crate::storage::storage::Storage;
 #[tokio::main]
 async fn main() {
     // dotenvy::dotenv().unwrap(); // 读取 .env
+    let address = env::var("ADDRESS").expect("DATABASE_URL must be set");
+
     tracing_subscriber::fmt().init();
     let state = AppState { storage: Arc::new(Storage::default().await) };
-    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    let acceptor = TcpListener::new(address).bind().await;
     Server::new(acceptor).serve(route::route(state)).await;
 }
 
