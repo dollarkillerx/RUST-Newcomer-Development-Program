0e39265d0425c54efe5c8fe60745dec85c2b2c55
Леони́д Ильи́ч Бре́жнев <google@gmail.com>
2024-08-19 11:54:38 +0900

    fix

--- s09_salvo/bash.sh
+++ s09_salvo/bash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 image_name=ru_system
-version=1.0.0
+version=1.0.1
 
 docker rmi -f github.tailbd724f.ts.net:5000/$image_name:$version
 docker build -f Dockerfile -t github.tailbd724f.ts.net:5000/$image_name:$version  .
